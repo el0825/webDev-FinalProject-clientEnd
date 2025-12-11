1fa4c87390de76b2dac70ff4e0e7c4b00a34aa06
Eric Lin <eric.lin67@myhunter.cuny.edu>
2025-12-10 22:02:42 -0500

    Exported EditStudentContainer

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -15,5 +15,5 @@ export { default as StudentContainer } from "./StudentContainer";
 export { default as NewStudentContainer } from "./NewStudentContainer";
 export { default as AddCampusContainer } from "./AddCampusContainer";
 export { default as EditCampusContainer } from "./EditCampusContainer";
-
+export { default as EditStudentContainer } from "./EditStudentContainer";
 
